4355398d69afe10513c8fdb170d68c79ec75fd0f
Adson <adson.oliveira.inf@gmail.com>
2024-02-09 11:07:02 -0300

    Primeiro commit para a Web

---
+++ screens/New.tsx
@@ -0,0 +1,20 @@
+import { StyleSheet, View, Image } from 'react-native';
+
+export default function New() {
+  return (
+    <View style={styles.container}>
+      <Image
+        source={require("../assets/Fundo.png")}
+      />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  }
+});
