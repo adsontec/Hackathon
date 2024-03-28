0c144477577a01a52d6eb3f0c69b7d62302a3424
Adson <adson.oliveira.inf@gmail.com>
2024-03-28 13:15:07 -0300

    Water

--- App.tsx
+++ App.tsx
@@ -33,17 +33,18 @@ export default function App() {
             <Text style={{color: focused ? '#2f32e3' : '#748c94'}}>Home</Text>
           </View>
           ) }} />
+        <Tab.Screen name="Alarm" component={Alarm} options={{ headerShown: false, tabBarIcon: ({ focused }) => (
+          <View style={{top: -20, position: 'absolute', backgroundColor: '#fff', borderRadius: 30, padding: 10, elevation: 5, shadowColor: '#062f6d', shadowOpacity: 1.0, shadowOffset: { width: 0, height: 1 } }}>
+            <MaterialIcons style={{color: focused ? '#2f32e3' : '#748c94'}} name="access-alarms" size={40} />
+          </View>
+          ) }} />
         <Tab.Screen name="Analysis" component={Analysis} options={{ headerShown: false, tabBarIcon: ({ focused }) => (
           <View style={{alignItems: 'center', justifyContent: 'center'}}>
             <MaterialIcons style={{color: focused ? '#2f32e3' : '#748c94'}} name="bar-chart" size={24} /> 
             <Text style={{color: focused ? '#2f32e3' : '#748c94'}}>Analysis</Text>
           </View>
           ) }} />
-        <Tab.Screen name="Alarm" component={Alarm} options={{ headerShown: false, tabBarIcon: ({ focused }) => (
-          <View style={{top: -20, position: 'absolute', backgroundColor: '#fff', borderRadius: 30, padding: 10, elevation: 5, shadowColor: '#062f6d', shadowOpacity: 1.0, shadowOffset: { width: 0, height: 1 } }}>
-            <MaterialIcons style={{color: focused ? '#2f32e3' : '#748c94'}} name="access-alarms" size={40} />
-          </View>
-          ) }} />
+        {/*
         <Tab.Screen name="Setting" component={Setting} options={{ headerShown: false, tabBarIcon: ({ focused }) => (
           <View style={{alignItems: 'center', justifyContent: 'center'}}>
             <FontAwesome style={{color: focused ? '#2f32e3' : '#748c94'}} name="gear" size={24} />
@@ -56,6 +57,7 @@ export default function App() {
             <Text style={{color: focused ? '#2f32e3' : '#748c94'}}>Profile</Text>
           </View>
           ) }}/>
+        */}
       </Tab.Navigator>
     </NavigationContainer>
   );
--- src/screens/Alarm/index.tsx
+++ src/screens/Alarm/index.tsx
@@ -1,12 +1,37 @@
 import { StatusBar } from 'expo-status-bar';
-import { Text, Image } from 'react-native';
+import { Text, Image, View, TouchableOpacity, Modal, Button } from 'react-native';
 import { Container } from './styles';
+import React, {useState} from "react"
+import { Calendar } from 'react-native-calendars';
 
-export function Alarm() {
+export function Alarm() { 
+  const [selectedDate, setSelectedDate] = useState<string | undefined>(undefined);
+
+  const onDayPress = (day: any) => {
+    setSelectedDate(day.dateString);
+  };
+
   return (
     <Container>
         <StatusBar style="auto" />
         <Image source={require("../../../assets/Fundo.png")} />
+          <TouchableOpacity  style={{position: 'absolute', width: 400, height: 200, margin: 5, backgroundColor: '#FFF', borderRadius: 12}}>
+          <Calendar
+            onDayPress={onDayPress}
+            markedDates={{
+              [selectedDate || '']: {
+                selected: true,
+                selectedColor: 'blue',
+              },
+            }}
+          />
+          </TouchableOpacity>
+        
+        {/*
+        <View style={{width: 400, height: 200, position:'absolute', backgroundColor: '#FFF', borderRadius: 12}}>
+        
+        </View>
+        */}
     </Container>
   );
 }
